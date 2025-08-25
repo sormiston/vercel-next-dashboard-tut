e4d8b948cc23ef6f29f57c073f6bbb26cd7b2068
sormiston <sean.david.ormiston@gmail.com>
2025-08-25 12:32:00 +0100

    Chap 12: server actions and mutating data

---
+++ src/app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,30 @@
+import Form from "@/src/ui/invoices/edit-form";
+import Breadcrumbs from "@/src/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/src/lib/data";
+
+export default async function InvoiceEditPage(props: {
+  params: Promise<{ id: string }>;
+}) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
--- src/ui/invoices/create-form.tsx
+++ src/ui/invoices/create-form.tsx
@@ -6,11 +6,12 @@ import {
   CurrencyDollarIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
+import { createInvoice } from "@/src/lib/actions";
 import { Button } from "@/src/ui/button";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
